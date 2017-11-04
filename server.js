f1f609d2b92b31e694d499dade7c355ba6ac077a
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-11-04 11:52:45 -0700

    updated links, all working on localhost

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 // =============================================================
 var currentTables = [
   {
-   customerName: "Ashley",
+   customerName: "Teera",
    customerPhone: "415-555-5555",
    customerEmail: "a@c.com",
    uniqueId: 123
@@ -65,11 +65,15 @@ var waitlist = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res){
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // Get all current tables
@@ -132,3 +136,65 @@ app.listen(PORT, function() {
   //console.log(currentTables);
   //console.log(waitlist);
 });
+
+//convert the html form data to json (we may not need to? not sure)
+//===============================================================
+
+
+//button functions
+//================================================================
+
+// function displayCurrentTables(){
+
+// }
+
+// function displayWaitlist(){
+
+//  // Here we get the location of the root page.
+// // We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+//   var currentURL = window.location.origin;
+
+//   // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
+//   $.ajax({url: currentURL + "/api/waitlist", method: "GET"})
+//       .done(function(waitlistData) {
+
+//           // Here we are logging the URL so we have access to it for troubleshooting
+//           console.log("------------------------------------");
+//           console.log("URL: " + currentURL + "/api/waitlist");
+//           console.log("------------------------------------");
+
+//           // Here we then log the NYTData to console, where it will show up as an object.
+//           console.log(waitlistData);
+//           console.log("------------------------------------")
+
+//           // Loop through and display each of the customers
+//           for (var i=0; i<waitlistData.length; i++){
+
+//               // Create the HTML Well (Section) and Add the table content for each reserved table
+//               var waitlistSection = $("<div>");
+//               waitlistSection.addClass('well');
+//               waitlistSection.attr('id', 'waitlistWell-' + i+1)
+//               $('#waitlistSection').append(waitlistSection);
+
+//               var tableNumber = i + 1;
+
+//               // Then display the remaining fields in the HTML (Section Name, Date, URL)
+//               $("#waitlistWell-"+ i+1).append('<h2><span class="label label-primary">' + tableNumber + "</span> | " + waitlistData[i].customerID + "</h2>");
+//           }
+//       });
+
+// }
+
+// function clearTables(){
+//   var currentURL = window.location.origin;
+//   $.ajax({url: currentURL + "/api/clear", method: "POST"})
+// }
+
+// $("#clear").on('click', function(){
+//   alert("Clearing tables...");
+//   clearTable();
+
+//   // Refresh the page after data is cleared
+//   location.reload();
+// })
+

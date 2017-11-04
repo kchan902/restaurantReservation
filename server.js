2acff2ab1cf3f8e0e10afb68bdf5e8e7cfbd127d
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-11-04 10:44:53 -0700

    ashley update with tables and waitlist

---
+++ server.js
@@ -0,0 +1,115 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Tables with people eating
+// =============================================================
+var currentTables = [
+  {
+   customerName: "Ashley",
+   customerPhone: "415-555-5555",
+   customerEmail: "a@c.com",
+   uniqueId: 123
+  },
+  {
+   customerName: "Karina",
+   customerPhone: "415-555-5555",
+   customerEmail: "a@c.com",
+   uniqueId: 124
+  },
+  {
+   customerName: "Chauncey",
+   customerPhone: "415-555-5555",
+   customerEmail: "a@c.com",
+   uniqueId: 125
+  }
+];
+
+// Waitlist for tables 
+//=============================================================
+
+var waitlist = [
+  {
+   customerName: "Ashley",
+   customerPhone: "415-555-5555",
+   customerEmail: "a@c.com",
+   uniqueId: 126
+  },
+  {
+   customerName: "Ashley",
+   customerPhone: "415-555-5555",
+   customerEmail: "a@c.com",
+   uniqueId: 127
+  },
+  {
+   customerName: "Ashley",
+   customerPhone: "415-555-5555",
+   customerEmail: "a@c.com",
+   uniqueId: 128
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Get all current tables
+app.get("/all", function(req, res) {
+  res.json(currentTables);
+});
+
+// Search for Specific Table (or all tables) - provides JSON
+app.get("/api/:currentTables?", function(req, res) {
+  var chosen = req.params.currentTables;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < currentTables.length; i++) {
+      if (chosen === currentTables[i].routeName) {
+        return res.json(currentTables[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(currentTables);
+});
+
+// Create New Waitlist - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newWaitlist = req.body;
+  newWaitlist.routeName = newWaitlist.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newWaitlist);
+
+  waitlist.push(newWaitlist);
+
+  res.json(newWaitlist);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
